ac5fafcc86061d87a559e408f39748d59710565b
unknown <carloseduardo_alencar@outlook.com>
2021-04-05 10:58:55 -0300

    Programa finalizado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sinal_DecisaoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Sinais ---\n");
+
+            Console.Write("Digite o numero desejado: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroDigitado <0 )
+            {
+                Console.WriteLine($"O {numeroDigitado} é negativo.");
+            }
+            else if (numeroDigitado == 0)
+            {
+                Console.WriteLine($"O numero {numeroDigitado} é zero.");
+            }
+            else
+            {
+              Console.WriteLine($"O numero {numeroDigitado} é positivo.");
+            }
+
+            Console.WriteLine("Obrigado por utilizar nosso programa!");
+        }
+    }
+}
